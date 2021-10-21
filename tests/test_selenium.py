2d9d8b355d13bdc49e87a204478a2281bceaf151
thetheatrenerd <jennicaprice@gmail.com>
2021-10-21 13:43:01 -0700

    Fixing?

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-browser = webdriver.Firefox()
+from time import sleep
+
+browser = webdriver.Chrome()
 browser.get("/index.html")
 bigBadButton = browser.find_element_by_id("clicky")
 mainText = browser.find_element_by_id("uh-oh").text
 newText = "YOU CLICKED THE BUTTON HOW COULD YOU"
 clicky.click()
-wait = WebDriverWait( browser, 5 )
+sleep(5)
 assert mainText == newText
+browser.close()
