9cd3b764a7dd63ef07dfcf55cb170049ca0c9e59
thetheatrenerd <jennicaprice@gmail.com>
2021-10-21 13:32:39 -0700

    Fixing test.

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,14 +1,34 @@
+import unittest
 from selenium import webdriver
-import time
+import requests
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--test-type")
-options.binary_location = "/usr/bin/chromium"
-driver = webdriver.Chrome(chrome_options=options)
-driver.get('/index.html')
+class CheckThePage(unittest.TestCase):
+    def click_and_check(self):
 
-# Click the button
-big_bad_button = driver.find_element_by_id('clicky')
-big_bad_button.click()
+        try:
+            self.driver.get('index.html')
+            self.driver.maximize_window()
+            self.driver.find_element_by_id('clicky').click()
 
+            WebDriverWait(self.driver, 5)
+            
+            mainText = self.driver.find_element_by_id('uh-oh')
+
+            newText = 'YOU CLICKED THE BUTTON HOW COULD YOU'
+            self.assertEqual(mainText, newText)
+
+            self.test_result = 'pass'
+
+        except AssertionError as e:
+            self.test_result = 'fail'
+            raise
+
+    def tearDown(self):
+        print("Done with session %s" % self.driver.session_id)
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
