4ee9f2fcea4d9d2d088eac1514f3c1a775feac32
thetheatrenerd <jennicaprice@gmail.com>
2022-02-17 09:42:50 -0800

    Test splitting

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -25,18 +25,7 @@ def dummy_test_1():
     num2 = 5
     num3 = 9
 
+    sleep(27)
+
     assert num1 + num2 == num3
-
-def dummy_test_2():
-    num1 = 27
-    num2 = 27
-
-    assert num1 == num2
-
-
-def dummy_test_3():
-    num1 = 2
-    num2 = 3
-
-    assert num1 + num2 == num2 + num1
 
