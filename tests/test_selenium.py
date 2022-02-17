99b76482b8bc0dff2c64d191d775ba002aee02ff
thetheatrenerd <jennicaprice@gmail.com>
2022-02-17 10:38:09 -0800

    All the way back to known working versions

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,13 +1,9 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 import sys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 def test_the_page():
     browser = webdriver.Chrome()
